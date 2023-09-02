7252e7135f564e3ea629cb3bb2b1c4465cffa753
pawang1745 <121444830+pawang1745@users.noreply.github.com>
2023-09-02 23:15:01 +0530

    code.py

---
+++ day2/code.py
@@ -0,0 +1,16 @@
+#Day2 challenge
+print("Getting to know you!")
+
+your_Name = input("What's your name?: ")
+food = input("What's your favourite food?: ")
+music = input("What's your favourite music?: ")
+place = input("Where do you live?: ")
+
+print("you are")
+print(your_Name)
+print("favourite food")
+print(food)
+print("favourite music")
+print(music)
+print("Living in")
+print(place)
