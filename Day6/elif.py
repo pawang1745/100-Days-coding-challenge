c78eb619fa8935be614e80da71b0d929269fb801
pawang1745 <121444830+pawang1745@users.noreply.github.com>
2023-09-06 20:20:15 +0530

    Create elif.py

---
+++ Day6/elif.py
@@ -0,0 +1,25 @@
+#elif uses
+print("Secure Login!")
+user_Name = input("Name: ")
+password = input("Password: ")
+if user_Name == "Pawan" and password == "17p":
+  print("Hi Pawan!")
+elif user_Name == "JO" and password == "yojo":
+  print("Hello JO!")
+else:
+  print("Nope Wrong Username..")#elif uses
+
+
+#Day 6 challenge
+print("my login system!!")
+print("++++++++++++++++")
+name = input("Username-> ")
+password = input("password-> ")
+if name == "hari" and password == "h56":
+  print("yo hari how was coding today?")
+elif name == "madan" and password == "m18":
+  print("How was your day?")
+elif name == "david" and password == "d7":
+  print("Have nice Day!!")
+else:
+  print("oh wrong credentials!!")
