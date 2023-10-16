9d1e8ef1307f9a5e490fc4953d8d6731105390e4
pawang1745 <121444830+pawang1745@users.noreply.github.com>
2023-10-16 21:50:42 +0530

    2d dict.py

---
+++ Day46/2d dict.py
@@ -0,0 +1,77 @@
+#2D Dictionaries
+"""
+clue = {}
+
+while True:
+  name = input("Name: ")
+  location = input("Location: ")
+  weapon = input("Weapon: ")
+
+  clue[name] = {"location": location, "weapon":weapon} #line 7
+
+  print(clue)
+"""
+print("=============================")
+
+def prettyPrint():
+  print()
+
+  for key, value in clue.items():
+    # moves along every 'key:subDictionary' pair and outputs the key (the name of the character).
+    print(key, end=": ")
+    for subKey, subValue in value.items():
+      # (nested) `for` loop moves along every subkey and subvalue in each subDictionary.
+      print(subKey, subValue, end=" | ")
+    print()
+
+print("==================================")
+clue = {}
+def prettyPrint():
+  print()
+
+  for key, value in clue.items():
+    # moves along every 'key:subDictionary' pair and outputs the key (the name of the character).
+    print(key, end=": ")
+    for subKey, subValue in value.items():
+      # (nested) `for` loop moves along every subkey and subvalue in each subDictionary.
+      print(subKey, subValue, end=" | ")
+    print()
+
+while True:
+  name = input("Name: ")
+  location = input("Location: ")
+  weapon = input("Weapon: ")
+
+  clue[name] = {"location": location, "weapon":weapon} 
+
+  prettyPrint()
+
+print("==============================")
+john = {"daysCompleted": 46, "streak": 22}
+janet = {"daysCompleted": 21, "streak": 21}
+erica = {"daysCompleted": 75, "streak": 6}
+
+courseProgress = {"John":john, "Janet":janet, "Erica":erica}
+
+print(courseProgress)
+
+print("===============================")
+import os, time
+
+mokedex = {}
+
+def prettyPrint():
+  print(f"Name\tType\tHP\tMP")
+  for key, value in mokedex.items():
+    print(f"""{key:^12}|{value["type"]:^10}|{value["hp"]:^6}|{value["mp"]:^6}""")
+
+while True:
+  print("Add your Beast!")
+  name = input("Name > ").title()
+  type = input("Type > ").title()
+  hp = int(input("HP > "))
+  mp = int(input("MP > "))
+  mokedex[name] = { "type": type, "hp": hp, "mp": mp}
+  print("----------")
+  print()
+  prettyPrint()
