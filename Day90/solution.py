247c02d0ee9d74edd8c585a817aa9f278336a523
PAWAN G <121444830+pawang1745@users.noreply.github.com>
2023-11-29 21:45:04 +0530

    solution.py

---
+++ Day90/solution.py
@@ -0,0 +1,14 @@
+import json, requests
+
+for i in range(10):
+  result = requests.get("https://randomuser.me/api/")
+  if result.status_code == 200:
+    user = result.json()
+
+  for person in user["results"]:
+    filename = f"""{person["name"]["first"].lower()}-{person["name"]["last"].lower()}.jpg"""
+    picture = requests.get(person["picture"]["medium"])
+    f =  open(filename, "wb")
+    f.write(picture.content)
+    f.close()
+    print(f"Saved {filename}")
